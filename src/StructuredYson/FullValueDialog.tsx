adc324c504cc328086c866bb5ee7ff13ffdd466f
xexys <xexys@yandex-team.ru>
2026-06-09 08:26:09 +0300

    fix(ReactUnipika): full value dialog lines breaks

--- src/StructuredYson/FullValueDialog.tsx
+++ src/StructuredYson/FullValueDialog.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import {Dialog, Flex} from '@gravity-ui/uikit';
+import React, {useState} from 'react';
+import {Dialog, Flex, SegmentedRadioGroup} from '@gravity-ui/uikit';
 import {cn} from '../utils/classname';
 import {FilteredText} from './FilteredText';
+
+import i18n from './i18n';
 
 import './FullValueDialog.scss';
 
@@ -17,17 +19,45 @@ export interface FullValueDialogProps {
 export function FullValueDialog(props: FullValueDialogProps) {
     const {onClose, text, starts, length} = props;
 
+    const [type, setType] = useState<'raw' | 'parsed'>('parsed');
+
     return (
         <Dialog open={true} onClose={onClose}>
-            <Dialog.Header caption={'Full value'} />
+            <Dialog.Header caption={i18n('full-value_title')} />
             <Dialog.Divider />
             <Dialog.Body>
                 <Flex direction="column" gap={2} width="70vw" maxHeight="80vh">
+                    <SegmentedRadioGroup
+                        className={block('full-value-radio-buttons')}
+                        options={[
+                            {value: 'parsed', content: i18n('full-value_parsed')},
+                            {value: 'raw', content: i18n('full-value_raw')},
+                        ]}
+                        onUpdate={setType}
+                        value={type}
+                    />
+
                     <div className={block('full-value')}>
-                        <FilteredText starts={starts} text={text} length={length} />
+                        {type === 'raw' ? (
+                            <FilteredText starts={starts} text={text} length={length} />
+                        ) : (
+                            <pre style={{whiteSpace: 'pre-wrap'}}>{getParsedFullValue(text)}</pre>
+                        )}
                     </div>
                 </Flex>
             </Dialog.Body>
         </Dialog>
     );
+}
+
+function getParsedFullValue(text: string) {
+    try {
+        return JSON.parse(text);
+    } catch {
+        try {
+            return JSON.parse(`"${text}"`);
+        } catch {
+            return text;
+        }
+    }
 }
